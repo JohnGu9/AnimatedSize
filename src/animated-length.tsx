b1ebb64fbd3fd90ea8a9d82fafb0a1042c3b060e
JohnGu9 <31389215+JohnGu9@users.noreply.github.com>
2022-10-13 09:31:41 +0800

    optimize performance

--- src/animated-length.tsx
+++ src/animated-length.tsx
@@ -5,13 +5,14 @@ export type Factor = number | string |
   null;    /** animate to Factor=1 and remove the length property from inline style sheet. For example: width: undefined */
 
 export function useAnimatedLength(factor: Factor, offsetLength: number | undefined, duration: number, notifyUpdate: () => unknown) {
-  const [auto, setAuto] = React.useState(factor === 'auto' || factor === null);
-
-  const expectLength = (() => {
-    if (factor === 'auto' || factor === null) {
+  const isAuto = factor === 'auto' || factor === null;
+  const [auto, setAuto] = React.useState(isAuto);
+  const length = (() => {
+    if (isAuto) {
       if (auto) return factor === null ? undefined : 'auto';
       else return offsetLength;
     }
+    if (auto) return offsetLength;
     if (typeof factor === 'string') return factor;
     if (offsetLength !== undefined) return factor * offsetLength;
     if (factor === 0) return 0;
@@ -19,24 +20,21 @@ export function useAnimatedLength(factor: Factor, offsetLength: number | undefin
     return undefined;
   })();
 
-  const length = (factor !== 'auto' && factor !== null && auto)
-    ? offsetLength
-    : expectLength;
-
-  const animating = useAnimatingOnChange(expectLength, duration, notifyUpdate);
-
+  const animating = useAnimatingOnChange(length, auto ? 0 : duration, notifyUpdate);
   React.useEffect(() => {
-    if (factor === 'auto' || factor === null) {
+    if (isAuto) {
       if (!animating) {
         setAuto(true);
       }
     } else {
       setAuto(false);
     }
-  }, [animating, factor]);
+  }, [animating, isAuto]);
 
   return length;
 }
+
+const { clearTimeout, setTimeout } = window;
 
 function useAnimatingOnChange(current: unknown, duration: number, notifyUpdate: () => unknown) {
   const state = React.useMemo(() => {
@@ -45,19 +43,16 @@ function useAnimatingOnChange(current: unknown, duration: number, notifyUpdate: 
   }, []);
 
   if (state.current !== current || state.duration !== duration) {
+    clearTimeout(state.animating);
+
     state.current = current;
     state.duration = duration;
-
-    window.clearTimeout(state.animating);
-
-    if (duration === 0) {
-      state.animating = undefined;
-    } else {
-      state.animating = window.setTimeout(() => {
+    state.animating = duration === 0
+      ? undefined
+      : setTimeout(() => {
         state.animating = undefined;
         notifyUpdate();
       }, duration);
-    }
   }
 
   return state.animating !== undefined;
