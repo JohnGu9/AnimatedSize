f7d9449b51206908f1c3d0d9f9bf2a84b0ec5ffb
JohnGu9 <johngumaster@outlook.com>
2025-03-04 17:18:03 +0800

    fix component argument type issue

--- src/create-component.tsx
+++ src/create-component.tsx
@@ -3,6 +3,6 @@ import React from "react";
 type E<T> = T extends React.DetailedHTMLProps<React.HTMLAttributes<infer E>, infer E> ? E : never;
 export type TagToElementType<T extends keyof JSX.IntrinsicElements> = E<JSX.IntrinsicElements[T]>;
 
-export function createComponent<Element, Props>(render: React.ForwardRefRenderFunction<Element, Props & Omit<React.HTMLProps<Element>, keyof Props>>) {
-    return React.forwardRef<Element, Props & Omit<React.HTMLProps<Element>, keyof Props>>(render);
+export function createComponent<Element, Props>(render: React.ForwardRefRenderFunction<Element, Props & Omit<React.HTMLProps<Element>, keyof Props | "ref">>) {
+    return React.forwardRef<Element, Props & Omit<React.HTMLProps<Element>, keyof Props | "ref">>(render);
 }
